f26bf444e77ad86f7788aa62125fad0db6064ace
Ricardo Lopez <therlg13@gmail.com>
2022-05-03 13:46:53 -0500

    Clase 3 | LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -28,3 +28,71 @@ console.log(epicPhrase);
 //es6
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+
+
+
+//Clase 3-------
+
+//Multilinea
+//antes de ES 6
+let lorem = "Qui consequatur, Commodi. Ipsum vel duis yet minima \n"
++ "otra frase epica que necesitamos."
+
+
+//Con ES 6
+let lorem2 = `otra frase que necesitamos
+ahora es otra frase
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+
+//Desestructuracion de elementos
+//Sin ES6
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+console.log(person.name, person.age);
+
+//Ahora con ES6
+let {name,age}=person;
+console.log(name, age);
+
+
+
+//Operador de propagacion (spread operator)
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+//Sin ES6
+let education1 =['Oscar', 'Julian', 'Ricardo','Valeria', 'Yesica', 'Camila']
+
+//Con ES6
+let education2 = ['David', ...team1, ...team2];
+
+console.log(education1, education2);
+
+
+//
+//var esta disponible de forma global
+//let solo disponible en el scope
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+//console.log(globalLet);
+
+//const permite establecer una variable a la cual no se le puede reasignar un valor, es una constante que nunca va  acambiar su valor
+const a='b';
+a = 'a';
+console.log(a);
