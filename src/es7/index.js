4bf1cbad2f1baffc108e6483402029b1bfcb96b9
Ricardo Lopez <therlg13@gmail.com>
2022-05-04 10:20:42 -0500

    Clase 6 | ¿Qué se implementó en ES7? | Includes y exponent

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+//Includes permite validar si hay un elemento dentro de un string o un arreglo
+let numbers = [1,2,3,7,8];
+if(numbers.includes(7)){
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log('No se encuentra');
+}
+
+
+//Para elevar a una potencia, se utiliza el **
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
