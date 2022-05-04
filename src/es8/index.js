06cf0fc7876b20ee4aed054c3a342231ac331e61
Ricardo Lopez <therlg13@gmail.com>
2022-05-04 11:02:34 -0500

    Clase 8 | Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -41,3 +41,36 @@ const data= {
     front:'Rick', 
     back: 'Rocoso', // Pueden existir mas valores y no pasa nada si se deja ahi la ,
 }
+
+
+
+
+
+//Clase 8
+const helloWorld = () => {  //Promesa
+    return new Promise((resolve, reject) => {
+        (true) //Se puede cambiar por false para ver el error
+            ? setTimeout(() => resolve('Hellow World'),3000) // ? nos dice que si se cumple nos demoraremos 3 segundos en entregar
+            : reject(new Error('Test Error')) //El : viene con el reject, esto cuando la promesa no se cumple
+    })
+};
+
+//Async marca una funcion como asincrona y su valor de retrono es una promesa
+const helloAsync = async () => {
+    const hello = await helloWorld();//Await pone en espera la ejecucion de la funcion mientras la promesa se resuelve
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => { //Para manejo de errores en este tipo de funciones
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+    //con try escribimos el codigo que esperamos se ejecute, en caso contrario, el codigo de catch sera el que se ejecutara
+}
+
+anotherFunction();
